21e5301fc6092597e86b9836939c8decb568cebb
saidiredddy <72790470+saidiredddy@users.noreply.github.com>
2021-10-05 19:02:28 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,17 +1,51 @@
-const express = require('express');
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
 
-const { register,
-        login,
-        getMe
- } = require('../controllers/auth');
+// Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-const router = express.Router()
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // Set token from Bearer token in header
+    token = req.headers.authorization.split(' ')[1];
+    // Set token from cookie
+  }
+ 
 
-const { protect } = require('../middlewares/auth')
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse('Not authorized to access this route', 401));
+  }
 
-router.post('/register',register)
-router.post('/login',login)
-router.get('/me', protect, getMe)
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    req.user = await User.findById(decoded.id);
 
-module.exports = router;
+    next();
+  } catch (err) {
+    return next(new ErrorResponse('Not authorized to access this route', 401));
+  }
+});
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
+
